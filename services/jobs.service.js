053fdc40551363609e1c4b1d8e30d80d89f3833b
Николай Лукиных <nlukinyh@cloudfactory.ru>
2026-09-21 23:06:19 +0300

    удалил ненужное поле

--- services/jobs.service.js
+++ services/jobs.service.js
@@ -12,12 +12,19 @@ const generalService = new GeneralService();
 class JobsService {
     constructor() {
         generalService.setBot(botService.bot);
+
+        console.log({
+            DAILY_TIME: process.env.DAILY_TIME,
+            CHOOSE_HOST_TIME: process.env.CHOOSE_HOST_TIME,
+            CLOSE_TASKS_TIME: process.env.CLOSE_TASKS_TIME,
+            CLOSE_TASKS_TIME_SECOND: process.env.CLOSE_TASKS_TIME_SECOND,
+            DELETE_OLD_VACATIONS: process.env.DELETE_OLD_VACATIONS,
+            DUTY_REMINDER_TIME: process.env.DUTY_REMINDER_TIME,
+            CHOOSE_NEW_BOT_NAME_TIME: process.env.CHOOSE_NEW_BOT_NAME_TIME,
+        });
     }
 
     dailyJob() {
-        console.log('-------');
-        console.log(JSON.stringify(process.env.DAILY_TIME));
-        console.log('-------');
         return cronService.createJob({
             cronTime: process.env.DAILY_TIME,
             onTick: () => generalService.daily(),
